bb09c0541d8cf3f5b6cf96d4542d0eb644e05ccd
bdeguio <77205811+bdeguio@users.noreply.github.com>
2025-01-29 13:19:55 -0800

    Update en-US.ts

--- app/reader/locales/en-US.ts
+++ app/reader/locales/en-US.ts
@@ -1,1 +1,41 @@
+export default {
+  'home.title': 'Home',
+  'home.share': 'Share',
+  'home.download': 'Download',
+  'home.download_sample_book': 'Download sample book',
+  'home.select': 'Select',
+  'home.cancel': 'Cancel',
+  'home.select_all': 'Select all',
+  'home.deselect_all': 'Deselect all',
+  'home.export': 'Export',
+  'home.import': 'Import',
+  'home.upload': 'Upload',
+  'home.delete': 'Delete',
 
+  'search.title': 'Search',
+  'search.files.result': '{n} results in {m} sections',
+
+  'theme.title': 'Theme',
+  'theme.background_color': 'Background Color',
+
+  'settings.title': 'Settings',
+  'settings.language': 'Language',
+  'settings.color_scheme': 'Color Scheme',
+  'settings.color_scheme.light': 'Light',
+  'settings.color_scheme.dark': 'Dark',
+  'settings.cache': 'Cache',
+  'settings.cache.clear': 'Clear',
+
+  'menu.copy': 'Copy',
+  'menu.search_in_book': 'Search in book',
+  'menu.create': 'Create',
+  'menu.delete': 'Delete',
+  'menu.update': 'Update',
+
+  'action.expand_all': 'Expand All',
+  'action.collapse_all': 'Collapse All',
+  'action.close': 'Close',
+  'action.clear': 'Clear',
+
+  untitled: 'Untitled',
+} as const;
